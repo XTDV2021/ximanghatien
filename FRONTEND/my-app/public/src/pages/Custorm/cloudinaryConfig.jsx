43b52e9f375307d1809fe5fb83cea8b74c93b363
XTDV2021 <94182550+XTDV2021@users.noreply.github.com>
2025-02-06 00:52:50 +0700

    home5

--- FRONTEND/my-app/public/src/pages/Custorm/cloudinaryConfig.jsx
+++
@@ -1,9 +0,0 @@
-import { Cloudinary } from "@cloudinary/url-gen";
-
-const cld = new Cloudinary({
-  cloud: {
-    cloudName: "Bipu", // Replace with your Cloudinary cloud name
-  },
-});
-
-export default cld;
--- FRONTEND/my-app/public/src/pages/UserAccount/UserDraft.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function UserDraft() {
-  return <div>UserDraft</div>;
-}
